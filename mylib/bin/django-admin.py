2fbc975d926056ebd71e3e609e985baf36fe46cc
Arthur Viana <arthur.ae@hotmail.com>
2019-06-02 13:02:12 -0300

    primeiro commit

---
+++ mylib/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/arthurviana/PycharmProjects/projeto-do-mini-curso/mylib/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
